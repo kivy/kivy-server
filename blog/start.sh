a3b9ea985412b5c36bb21368def0c74bfc6bfa42
dessant <git@rmn.space>
2017-01-02 22:49:21 +0200

    tini: bump version to 0.13.2

--- blog/start.sh
+++ blog/start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/cron.d/feed-update ]; then
   cat << EOF > /etc/cron.d/feed-update
-*/10 * * * * root /usr/bin/curl --silent http://blog.kivy.org/?update_feedwordpress=1
+*/10 * * * * root /usr/bin/curl --silent https://blog.kivy.org/?update_feedwordpress=1
 
 EOF
   chmod 0644 /etc/cron.d/feed-update
