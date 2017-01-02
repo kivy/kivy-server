79776cb9d4a80f72bcd9b2d2daa32e702559ea06
dessant <git@rmn.space>
2017-01-02 23:28:24 +0200

    piwik: update config

--- piwik/common.config.ini.php
+++ piwik/common.config.ini.php
@@ -1,26 +1,27 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
 
 [General]
-enable_processing_unique_visitors_multiple_sites = 1
-anonymous_user_enable_use_segments_API = 0
-adding_segment_requires_access = "admin"
 default_day = today
 assume_secure_protocol = 1
-overlay_disable_framed_mode = 1
 api_service_url = https://api.piwik.org
+anonymous_user_enable_use_segments_API = 0
 
-enable_trusted_host_check = 1
 trusted_hosts[] = kivy.org
 trusted_hosts[] = pw.kivy.org
 trusted_hosts[] = blog.kivy.org
 
+overlay_disable_framed_mode = 1
+
 enable_auto_update = 0
 enable_update_communication = 0
-piwik_pro_ads_enabled = 0
+piwik_professional_support_ads_enabled = 0
 
 enable_processing_unique_visitors_year = 1
 enable_processing_unique_visitors_range = 1
 enable_processing_unique_visitors_multiple_sites = 1
+
+minimum_memory_limit = -1
+minimum_memory_limit_when_archiving = -1
 
 [Tracker]
 enable_fingerprinting_across_websites = 1
